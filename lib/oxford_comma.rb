ebf36610717d3333e41cd61f53e0ea87340bf0a0
christianalim <christianalim1@gmail.com>
2019-06-01 13:32:47 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+    if array.count == 1
+        return array[0]
+    elsif array.count == 2
+        return "#{array[0]} and #{array[1]}"
+    else
+        lastelement= ", and #{array.last}"
+        arraynoand = array.tap(&:pop).join(", ")
+        return arraynoand + lastelement
+    end
 
 end
