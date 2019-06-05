4ef132d80390db82704a603edd7ffdfe8caca0c6
Grace Tan <gracetan37@gmail.com>
2019-06-05 16:35:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+if array.length == 1
+  array.join
+  elsif array.length == 2 
+  array.join(" and ")
+  elsif array.length == 3
+  new_string = array[0..1].join(", ")
+  new_string << ", and #{array.pop}"
+  elsif array.length > 3
+ last_string = ", and #{array.pop}"
+ newer_string = array.join(", ")
+ newer_string << last_string 
+ 
+end
 end
