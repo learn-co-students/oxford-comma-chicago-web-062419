47787af347e4bc12032f556d865338f4b37fa877
Amanda Treutler <amandathedev@gmail.com>
2019-06-09 16:27:04 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#     if array.length == 2
+#         array.join(" and ")
+#     elsif array.length > 2
+#         # https://gistpages.com/posts/ruby_arrays_insert_append_length_index_remove
+#         array.insert(-2, "and")
+#         array[0..-2].join(", ")
+#     else
+#         array.join
+#     end
+# end
 
-end
+
+# https://stackoverflow.com/questions/56518068/how-to-turn-an-array-of-strings-into-a-list-with-an-oxford-comma-using-array-met
+def oxford_comma(array)
+    if array.length < 3
+        return array.join(' and ')
+    end
+    array[-1] = "and " + array[-1]
+    array.join(', ')
+  end 
