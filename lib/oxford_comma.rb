068452a994d5f3280e4843c47d6403b6d4beba12
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-18 19:09:51 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length > 2
+  a = array.pop()
+  array.join(", ").concat(", and ") + a
+elsif array.length == 2
+  a = array.pop()
+  array.join(", ").concat(" and ") + a
+else
+  array.join()
+end
 end
